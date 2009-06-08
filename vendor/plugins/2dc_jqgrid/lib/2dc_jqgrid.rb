e8a9a548536aa95f4a78914594bc8dbae3866301
Anthony Heukmes <anthony.heukmes@skynet.be>
2009-06-08 17:10:56 +0200

    Return true/false in selection handler

--- vendor/plugins/2dc_jqgrid/lib/2dc_jqgrid.rb
+++ vendor/plugins/2dc_jqgrid/lib/2dc_jqgrid.rb
@@ -22,7 +22,9 @@ module ActionView
       options[:sort_column] = "id" if options[:sort_column].blank?
       options[:sort_order] = "asc" if options[:sort_order].blank?
       options[:height] = "150" if options[:height].blank?
-      options[:error_handler] = 'null' if options[:error_handler].blank?      
+      options[:error_handler] = 'null' if options[:error_handler].blank?
+      options[:error_handler_return_value] = options[:error_handler]
+      options[:error_handler_return_value] = "true;" if options[:error_handler_return_value] == 'null'
       options[:inline_edit_handler] = 'null' if options[:inline_edit_handler].blank?      
 
       options[:add] = (options[:add].blank?) ? "false" : options[:add].to_s    
@@ -235,9 +237,9 @@ module ActionView
         #{multihandler}
         #{selection_link}
         jQuery("##{id}").navGrid('##{id}_pager',{edit:#{edit_button},add:#{options[:add]},del:#{options[:delete]},search:false,refresh:true},
-        {afterSubmit:function(r,data){return #{options[:error_handler]}(r,data,'edit');}},
-        {afterSubmit:function(r,data){return #{options[:error_handler]}(r,data,'add');}},
-        {afterSubmit:function(r,data){return #{options[:error_handler]}(r,data,'delete');}
+        {afterSubmit:function(r,data){return #{options[:error_handler_return_value]}(r,data,'edit');}},
+        {afterSubmit:function(r,data){return #{options[:error_handler_return_value]}(r,data,'add');}},
+        {afterSubmit:function(r,data){return #{options[:error_handler_return_value]}(r,data,'delete');}
         })
         .navButtonAdd("##{id}_pager",{caption:"Search",title:"Toggle Search",buttonimg:'/images/jqgrid/search.png',
         	onClickButton:function(){ 
